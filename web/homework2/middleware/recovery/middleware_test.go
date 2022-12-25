12461a16358a82eb60c66614eabeeacddef5be36
Zen <zenchan@kktv.me>
2022-12-25 08:36:03 +0800

    copy from assignment template

---
+++ web/homework2/middleware/recovery/middleware_test.go
@@ -0,0 +1,29 @@
+package recovery
+
+import (
+	"log"
+	"testing"
+
+	web "github.com/oreo0725/geektime-go-camp/web/homework2"
+)
+
+func TestMiddlewareBuilder_Build(t *testing.T) {
+	s := web.NewHTTPServer()
+	s.Get("/user", func(ctx *web.Context) {
+		ctx.RespData = []byte("hello, world")
+	})
+
+	s.Get("/panic", func(ctx *web.Context) {
+		panic("闲着没事 panic")
+	})
+
+	s.Use((&MiddlewareBuilder{
+		StatusCode: 500,
+		ErrMsg:     "你 Panic 了",
+		LogFunc: func(ctx *web.Context) {
+			log.Println(ctx.Req.URL.Path)
+		},
+	}).Build())
+
+	s.Start(":8081")
+}
